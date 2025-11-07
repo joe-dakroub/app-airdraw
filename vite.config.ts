acbc5c24b8823e872a1efae0f4b07bbe2470b256
joe.dakroub <joe.dakroub@me.com>
2025-11-07 11:37:29 -0700

    Configure for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,10 @@ import { defineConfig } from 'vite'
 import mkcert from 'vite-plugin-mkcert'
 
 export default defineConfig({
+  base: '/app-airdraw/',
+  build: {
+    target: 'esnext',
+  },
   server: {
     https: true,      // enables HTTPS
     host: '0.0.0.0',  // allows access from other devices (e.g. iPhone)
